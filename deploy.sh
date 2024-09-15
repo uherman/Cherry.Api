0544e0e2bb3b76a4a8ed602ba3abf07694c8e0f6
Herman Brunberg <herman.brunberg@mio.se>
2024-09-16 00:03:04 +0200

    ci: add deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 # Description: Build and push the docker image and deploy the api to the k3s cluster
 
+dotnet publish -c Release
+
 # Build and push the docker image
 docker build . -t uherman/cherry-api
 docker push uherman/cherry-api
 
 # Deploy the api to the k3s cluster
 k3s apply -f deploy/deployment.yml
+k3s rollout restart deployment cherry-api
--- proxy/deploy.sh
+++ proxy/deploy.sh
@@ -1,8 +1,11 @@
 # Description: Build and push the docker image and deploy the proxy service to the k3s cluster
 
+dotnet publish -c Release
+
 # Build and push the docker image
 docker build . -t uherman/cherry-proxy
 docker push uherman/cherry-proxy
 
 # Deploy the proxy service to the k3s cluster
 k3s apply -f deployment.yml
+k3s rollout restart deployment cherry-proxy
